b26be606a4c7e1e9be0297404061154f0c02f144
Umayr Shahid <umayrr@hotmail.co.uk>
2016-03-16 16:17:29 +0500

    Add logging and refactor summary handler

--- server.go
+++ server.go
@@ -1,31 +1,37 @@
 package main
+
 import (
-	"github.com/gorilla/mux"
-	"summary-api/summary"
-	"log"
+	"os"
 	"net/http"
 	"encoding/json"
-	)
+	"summary-api/summary"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+)
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/summary", Summary).Methods("POST")
 
-	log.Print(http.ListenAndServe(":8080", router))
+	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
+	http.ListenAndServe(":8080", handlers.RecoveryHandler()(loggedRouter))
 }
 
 func Summary(w http.ResponseWriter, r *http.Request) {
-	a := &summary.Article{}
-	err := json.NewDecoder(r.Body).Decode(a)
+	var (
+		a = &summary.Article{}
+		err error = nil
+	)
+	err = json.NewDecoder(r.Body).Decode(a)
 	if err != nil {
 		panic(err)
 	}
 
-	s, e := json.Marshal(a.Summary())
-	if e != nil {
-		panic(e)
+	s, err := json.Marshal(summary.Generate(a))
+	if err != nil {
+		panic(err)
 	}
-	        w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	w.Write(s)
 }
