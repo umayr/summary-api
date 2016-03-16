980275289ac90228544866dbb5a6282b945e268c
Umayr Shahid <umayrr@hotmail.co.uk>
2016-03-16 16:20:45 +0500

    Refactor result type and summary functions

--- summary/result.go
+++ summary/result.go
@@ -1,11 +1,4 @@
 package summary
-import "regexp"
-
-type Analysis struct {
-	OriginalWordCount int
-	SummaryWordCount  int
-	Ratio             float32
-}
 
 type Result struct {
 	Title    string `json:"title"`
@@ -22,16 +15,4 @@ func (r *Result) Analyze(src *Article) {
 	a["natural"] = analyze(src.Content, r.Natural)
 
 	r.Analysis = a
-}
-
-func wordCount(s string) int {
-	return len(regexp.MustCompile("\\w+").FindAllString(s, -1))
-}
-
-func analyze(c string, s string) *Analysis {
-	wc := wordCount(c)
-	ws := wordCount(s)
-	r := float32(100 * ws) / float32(wc)
-
-	return &Analysis{wc, ws, r}
 }
--- summary/summary.go
+++ summary/summary.go
@@ -1,5 +1,44 @@
 package summary
 
-func Generate(a Article) *Result {
-	return a.Summary()
+import (
+	"strings"
+	"github.com/urandom/text-summary/summarize"
+	"github.com/JesusIslam/tldr"
+)
+
+const (
+	MaxSentences = 4
+	IdealWordCount = 75
+)
+
+func Generate(a *Article) *Result {
+	var (
+		cl = make(chan string)
+		cs = make(chan string)
+	)
+
+	go lexRank(cl, a.Content);
+	go natural(cs, a.Title, a.Content)
+
+	r := &Result{}
+	r.LexRank = <-cl
+	r.Natural = <-cs
+	r.Title = a.Title
+
+	r.Analyze(a)
+	return r;
+}
+
+// LexRank Algorithm
+func lexRank(c chan string, content string) {
+	t := tldr.New()
+	td, _ := t.Summarize(content, MaxSentences)
+	c <- td;
+}
+
+// Natural Language Processing Algorithm
+func natural(c chan string, title string, content string) {
+	s := summarize.NewFromString(title, content)
+	s.IdealWordCount = IdealWordCount
+	c <- strings.Join(s.KeyPoints(), "")
 }
